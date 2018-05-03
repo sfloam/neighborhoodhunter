a8adea31c0b32338597bb798704473061ea02644
Harivadhana <harivadhana@gmail.com>
2018-05-03 10:00:44 -0400

    RebuildJSON.java with SoHo

--- src/RebuildJSON.java
+++ src/RebuildJSON.java
@@ -15,13 +15,13 @@ public class RebuildJSON {
 		Map<String, Values> fromCSV = new HashMap<>();
 		Map<String, Integer> uniqueNeighborhoods = new HashMap<>();
 		BufferedReader br = new BufferedReader(
-				new FileReader(new File("C:/Hari/BDAD/JSON/New.csv")));
+				new FileReader(new File("src/Output_H.0.35_C.0.4_S.0.25.csv")));
 		br.readLine();
 		String line;
 		while ((line = br.readLine()) != null) {
 			String[] words = line.split(",");
 			Values values = fromCSV.get(words[0]) != null ? fromCSV.get(words[0]) : new Values();
-			values.rank = words[3];
+			values.setRank(words[1], words[3]);
 			values.score = words[4];
 			values.setPrice(words[1], words[2]);
 			fromCSV.put(words[0], values);
@@ -30,7 +30,7 @@ public class RebuildJSON {
 		System.out.println();
 
 		String json = "";
-		br = new BufferedReader(new FileReader(new File("C:/Hari/BDAD/JSON/nyc.geojson")));
+		br = new BufferedReader(new FileReader(new File("src/nyc.geojson")));
 		while ((line = br.readLine()) != null) {
 			json += line;
 		}
@@ -51,12 +51,9 @@ public class RebuildJSON {
 			result += "\"geometry\":" + feature.get("geometry") + "},";
 		}
 		result = result.substring(0, result.length() - 1) + "]}";
-		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("C:/Hari/BDAD/JSON/result_4.json")));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("src/result.json")));
 		bw.write(result);
 		bw.close();
-		for(Map.Entry<String, Integer> entry : uniqueNeighborhoods.entrySet()) {
-			System.out.println(entry.getKey());
-		}
 		System.out.println("!!!");
 	}
 }
@@ -64,6 +61,7 @@ public class RebuildJSON {
 class Values {
 
 	private String coopsWalkup = "-1", oneFamily = "-1", coopsElevator = "-1", twoFamily = "-1", threeFamily = "-1", condosElevator = "-1", condosWalkup = "-1";
+	private String coopsWalkupRank = "-1", oneFamilyRank = "-1", coopsElevatorRank = "-1", twoFamilyRank = "-1", threeFamilyRank = "-1", condosElevatorRank = "-1", condosWalkupRank = "-1";
 	public String rank = "-1", score = "-1";
 
 	public void setPrice(String family, String price) {
@@ -92,15 +90,41 @@ class Values {
 		}
 	}
 	
+	public void setRank(String family, String rank) {
+		switch (family) {
+		case "Coop_Walkup":
+			coopsWalkupRank = rank;
+			break;
+		case "One_Family":
+			oneFamilyRank = rank;
+			break;
+		case "Coop_Elevator":
+			coopsElevatorRank = rank;
+			break;
+		case "Two_Family":
+			twoFamilyRank = rank;
+			break;
+		case "Three_Family":
+			threeFamilyRank = rank;
+			break;
+		case "Condo_Elevator":
+			condosElevatorRank = rank;
+			break;
+		case "Condo_Walkup":
+			condosWalkupRank = rank;
+			break;
+		}
+	}
+	
 	@Override
 	public String toString() {
-		return "\"Coop_Walkup" + "\":{\"Rank\":" + rank + ",\"Score\":" + score + ",\"Price\":" + coopsWalkup + "}," +
-				"\"One_Family" + "\":{\"Rank\":" + rank + ",\"Score\":" + score + ",\"Price\":" + oneFamily + "}," +
-				"\"Coop_Elevator" + "\":{\"Rank\":" + rank + ",\"Score\":" + score + ",\"Price\":" + coopsElevator + "}," + 
-				"\"Two_Family" + "\":{\"Rank\":" + rank + ",\"Score\":" + score + ",\"Price\":" + twoFamily + "}," + 
-				"\"Three_Family" + "\":{\"Rank\":" + rank + ",\"Score\":" + score + ",\"Price\":" + threeFamily + "}," + 
-				"\"Condo_Elevator" + "\":{\"Rank\":" + rank + ",\"Score\":" + score + ",\"Price\":" + condosElevator + "}," + 
-				"\"Condo_Walkup" + "\":{\"Rank\":" + rank + ",\"Score\":" + score + ",\"Price\":" + condosWalkup + "},";
+		return "\"Coop_Walkup" + "\":{\"Rank\":" + coopsWalkupRank + ",\"Score\":" + score + ",\"Price\":" + coopsWalkup + "}," +
+				"\"One_Family" + "\":{\"Rank\":" + oneFamilyRank + ",\"Score\":" + score + ",\"Price\":" + oneFamily + "}," +
+				"\"Coop_Elevator" + "\":{\"Rank\":" + coopsElevatorRank + ",\"Score\":" + score + ",\"Price\":" + coopsElevator + "}," + 
+				"\"Two_Family" + "\":{\"Rank\":" + twoFamilyRank + ",\"Score\":" + score + ",\"Price\":" + twoFamily + "}," + 
+				"\"Three_Family" + "\":{\"Rank\":" + threeFamilyRank + ",\"Score\":" + score + ",\"Price\":" + threeFamily + "}," + 
+				"\"Condo_Elevator" + "\":{\"Rank\":" + condosElevatorRank + ",\"Score\":" + score + ",\"Price\":" + condosElevator + "}," + 
+				"\"Condo_Walkup" + "\":{\"Rank\":" + condosWalkupRank + ",\"Score\":" + score + ",\"Price\":" + condosWalkup + "},";
 	}
 
 }
